7ca48a5eb5a078755052095b547e1f450718328f
Pipirongcha <pipirongcha0x0@gmail.com>
2025-01-22 12:16:00 +0900

    GameScene UI: Add Skill(Q,W,E,R,Rolling) Description

---
+++ Assets/_Raiders/Scripts/HMJ/SkillManager/Explane/QskillUI.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+
+public class QskillUI : MonoBehaviour
+{
+    [SerializeField]
+    GameObject explane;
+    [SerializeField]
+    TextMeshProUGUI skillKey;
+    [SerializeField]
+    TextMeshProUGUI name;
+    [SerializeField]
+    TextMeshProUGUI description;
+
+    private void Start()
+    {
+        //skillKey.text = $"{KeySetting.keys[KeyAction.Q]}";
+
+    }
+    public void PointerEnter()
+    {
+        explane.SetActive(true);
+        //skillKey.text = $"{KeySetting.keys[KeyAction.Q]}";
+        name.text = $"매직 불릿 (쿨타임 3초)";
+        description.text = $"{Q.Instance.UseMp}MP를 소모하여, 정면으로 {Q.CalculatingDamage()} 만큼의 위력을 갖는 마법 총알을 2초간 발사한다. 제어불가.";
+    }
+
+    public void PointerExit()
+    {
+        explane.SetActive(false);
+    }
+}
---
+++ Assets/_Raiders/Scripts/HMJ/SkillManager/Explane/RollingUI.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+
+public class RollingUI : MonoBehaviour
+{
+    [SerializeField]
+    GameObject explane;
+    [SerializeField]
+    TextMeshProUGUI skillKey;
+    [SerializeField]
+    TextMeshProUGUI name;
+    [SerializeField]
+    TextMeshProUGUI description;
+
+    private void Start()
+    {
+        //skillKey.text = $"{KeySetting.keys[KeyAction.Space]}";
+        
+    }
+    public void PointerEnter()
+    {
+        explane.SetActive(true);
+        //skillKey.text = $"{KeySetting.keys[KeyAction.Space]}";
+        name.text = $"구르기 (쿨타임 3초)";
+        description.text = $"마우스 커서 방향으로 재빠르게 굴러 이동한다. \n시전 중 무적.";
+    }
+
+    public void PointerExit()
+    {
+        explane.SetActive(false);
+    }
+}
