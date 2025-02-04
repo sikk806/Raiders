711ffff8d15c1990ad65b88390e4d372ab14c8ff
sikk806 <sikk806@gmail.com>
2025-02-05 04:27:48 +0900

    Load, Win, Lose Need To Test

--- Assets/_Raiders/Scripts/KJS/UI/SceneLoader.cs
+++ Assets/_Raiders/Scripts/KJS/UI/SceneLoader.cs
@@ -1,13 +1,31 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class SceneLoader : MonoBehaviour
 {
     public static SceneLoader Instance { get; private set; }
+
     public Image FadeCanvas;
+    public TMP_Text Description;
+    public Slider ProgressBar;
+    public TMP_Text Percentage;
+
     float fadeDuration = 1.0f;
+    List<String> descriptions = new List<string>
+    {
+        "보스 클리어를 응원... 해드려야 하나요?",
+        "이걸 도전해 보신다구요? 대단하시네요.",
+        "자, 마우스를 잡고 왼쪽 버튼을 눌러보세요. 이게 클릭이랍니다.",
+        "어.. 음.. 네.. 열심히 잡아보세요..!",
+        "뿡. 앗.. 실수ㅎ",
+        "분수대 근처에서 상호작용 키를 누르면 패시브를 얻을 수 있어요!\n그런데 패시브 없이 싸울 수 없나요...? 저런..."
+    };
 
     void Awake()
     {
@@ -25,6 +43,7 @@ public class SceneLoader : MonoBehaviour
     {
         if (FadeCanvas == null) return; // 안전 체크
         FadeCanvas.color = new Color(0, 0, 0, 1); // 시작 시 완전 검은 화면
+        ProgressBar.gameObject.SetActive(false);
         StartCoroutine(FadeIn());
     }
 
@@ -36,18 +55,30 @@ public class SceneLoader : MonoBehaviour
     IEnumerator Transition(string sceneName)
     {
         FadeCanvas.gameObject.SetActive(true);
+        ProgressBar.gameObject.SetActive(true);
         yield return StartCoroutine(FadeOut());
 
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
         asyncLoad.allowSceneActivation = false;
 
+
+        ProgressBar.value = 0f;
+        Percentage.text = "0%";
+
         while (asyncLoad.progress < 0.9f)
         {
+            ProgressBar.value = asyncLoad.progress;
+            Percentage.text = (asyncLoad.progress * 100).ToString() + "%";
             yield return null;
         }
 
+        ProgressBar.value = 1f;
+        Percentage.text = "100%";
         yield return new WaitForSeconds(1f);
 
+        Description.text = "";
+        Percentage.text = "";
+        ProgressBar.gameObject.SetActive(false);
         asyncLoad.allowSceneActivation = true;
         yield return null;
 
@@ -58,7 +89,6 @@ public class SceneLoader : MonoBehaviour
     {
         if (FadeCanvas == null) yield break; // 안전 체크
         Color color = FadeCanvas.color;
-
         for (float t = fadeDuration; t >= 0; t -= Time.deltaTime)
         {
             color.a = Mathf.Clamp01(t / fadeDuration); // 보정 추가
@@ -73,6 +103,8 @@ public class SceneLoader : MonoBehaviour
     IEnumerator FadeOut()
     {
         if (FadeCanvas == null) yield break; // 안전 체크
+
+        Description.text = descriptions[Random.Range(0, 6)];
         Color color = FadeCanvas.color;
         for (float t = 0; t <= fadeDuration; t += Time.deltaTime)
         {
